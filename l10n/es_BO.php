af3d83e1890db2fcbcb2973f2d025143a9059dab
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-05-17 01:57:38 -0400

    [tx-robot] updated from transifex

--- l10n/es_BO.php
+++ l10n/es_BO.php
@@ -1,10 +1,7 @@
 <?php
 $TRANSLATIONS = array(
-"Share" => "تقسیم",
-"Error" => "ایرر",
 "_%n folder_::_%n folders_" => array("",""),
 "_%n file_::_%n files_" => array("",""),
-"_Uploading %n file_::_Uploading %n files_" => array("",""),
-"Delete" => "حذف کریں"
+"_Uploading %n file_::_Uploading %n files_" => array("","")
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
